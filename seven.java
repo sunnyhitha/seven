1d3429b95a853f69bea25d02b000a4971cda9c54
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-16 12:15:56 +0530

    Create seven.java

---
+++ seven.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		if(a>=b&&a<=c)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}// your code goes here
+	}
+}
